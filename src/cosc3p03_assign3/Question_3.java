6bb67eb5043c3683270e60bd57cd1053b61add6c
unknown <pw12nb@COSC-J31016.msad.cosc.brocku.ca>
2016-03-21 13:08:34 -0400

    added the printouts

--- src/cosc3p03_assign3/Question_3.java
+++ src/cosc3p03_assign3/Question_3.java
@@ -1,7 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * A one-way street [0, inf) of infinite length has a bus stop every mile. A
+   passenger is charged according to the number of miles he rides on the bus. The bus cannot travel
+   more than m miles (m ≥ 1) each time (in other words, the bus can travel 1 mile, 2 miles,..., m
+   miles non-stop. In case a rider wants to travel more than m miles, the trip has to be taken by
+   several bus rides. For example, a trip of m + 3 miles can be taken as bus rides of 1 miles, 3 miles,
+   and m − 1 miles; or m miles, followed by 3 miles, etc. Similarly, a trip of m − 1 miles could be a
+   ride of m − 1 miles, or a ride of 2 miles followed by m − 3 miles, or even m − 1 rides of 1 mile each.
  */
 package cosc3p03_assign3;
 
